05536a0a4daeefd30c6771fa372ced0bfafa7b45
Diana Cano <dcano31@stu.southtexascollege.edu>
2025-11-08 15:37:09 -0600

    installed cors

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import route from "./route/employeeRoute.js";
+import cors from "cors"
 
 dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors())
+
 
 const PORT = process.env.PORT || 5000;
 const MONGOURI = process.env.MONGO_URL;
